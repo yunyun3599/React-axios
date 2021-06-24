7d868ee47d1bab5c6302341b5eff1988e4642358
YJ <yunyun3599@gmail.com>
2021-06-24 14:15:09 +0900

    news-viewer made with axios, promise, external API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState, useCallback} from 'react';
+import axios from 'axios';
+import {Route} from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
+//import NewsList from './components/NewsList';
+//import Categories from './components/Categories';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  //const [data,setData] = useState(null);
+  //const [category, setCategory] = useState('all');
+  //const onSelect = useCallback(category => setCategory(category), []);
+
+  /*const onClick = () => {
+    axios.get("https://newsapi.org/v2/top-headlines?country=kr&apiKey=d47dee16243d40659f8345fecf8fd2f4")
+    .then(response => {
+      setData(response.data);
+    });
+  };
+  */
+
+  /* async 적용한 코드
+  const onClick = async () => {
+    try{
+      const response = await axios.get("https://newsapi.org/v2/top-headlines?country=kr&apiKey=d47dee16243d40659f8345fecf8fd2f4");
+      setData(response.data);
+    }
+    catch(e) {
+      console.log(e);
+    }
+  };
+*/
+  /*return (
+    <>
+    <Categories category={category} onSelect={onSelect}/>
+    <NewsList category={category}/>
+    </>
+  );*/
+  return(
+    <Route path = '/:category?' component={NewsPage} />
   );
-}
+};
 
 export default App;
